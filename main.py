482a092ee2019ad92b99b66f255b7cabb31c4f78
Nathan Costa <112785244+n-kyu@users.noreply.github.com>
2024-03-18 17:54:31 -0300

    feat: Bocchi bot v0.1

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+from pyexpat import model
+import discord
+import openai
+from discord.ext import commands
+from dotenv import load_dotenv
+
+
+load_dotenv()
+DISCORD_BOT_TOKEN=os.getenv("DISCORD_BOT_TOKEN")
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+
+
+intents = discord.Intents.default()
+intents.messages = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+openai.api_key=OPENAI_API_KEY
+
+async def search_history_channel(channel, limit=10):
+    messages_list = []
+    async for message in channel.history(limit=limit, oldest_first=True):
+
+        messages_list.append({
+            "role": "user" if message.author.id != bot.user.id else "system",
+            "content": message.content
+        })
+    return messages_list
+
+
+def asking_gpt(gptmessage):
+
+    response = openai.ChatCompletion.create(
+        messages=gptmessage,
+        model="gpt-3.5-turbo-16k",
+        temperature=0.9,
+        max_tokens=400
+    )
+
+    return response.choices[0].message.content
+
+
+@bot.event
+async def on_ready():
+        print(f"{bot.user.name} is ready!")
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    async with message.channel.typing():
+        history_messages = await search_history_channel(message.channel)
+        response = asking_gpt(history_messages)
+
+        await message.reply(response)
+
+    await bot.process_commands(message)
+
+
+
+bot.run(DISCORD_BOT_TOKEN)
